74c79e6474fb79bfe7218986c286aed1070327bb
Taylor <tbaustin1992@gmail.com>
2018-04-19 13:24:54 -0500

    add fs to write folder

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import axios from 'axios';
 import zipcodes from 'zipcodes';
 
 export default async function() {
-  return Object.keys(zipcodes.states.abbr).map(async state => {
+  // if folder does not exist create JSON folder
+  if (!fs.readdir(__dirname, '/dist/JSON')) {
+    await fs.mkdir(__dirname + '/dist/JSON');
+  }
+
+  // Loop through states and pass into dealer API
+  Object.keys(zipcodes.states.abbr).map(async state => {
     const result = await axios.get(
       `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/state/${state}`
     );
-    // await fs.mkdir(__dirname + '/dist/JSON');
-    // if (fs.readdir(__dirname + '/dist/JSON')) {
-    // loop through zip codes here
-    // make fetch call for each zip code here
-    // fs.writeFile(`${__dirname}/dist/JSON/${zip}.JSON`, res.data.zip) for each zipcode
-    // }
-    console.log(result.data.dealers);
-    return result.data.dealers;
+    // Loop through dealers and create file name by zip with contents of dealer
+    // This will override older files, so essentailly "updating" it as well
+    result.data.dealers.map(async dealer => {
+      fs.writeFile(`${__dirname}/dist/JSON/${dealer.zip}.JSON`, dealer);
+    });
   });
 }
