24acc621c1b69d64d104ca88c5fa2d23b0ee83cf
Taylor <tbaustin1992@gmail.com>
2018-04-19 12:15:47 -0500

    change code into src

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import fs from 'fs-extra';
 
 export default async function() {
-  await fs.mkdir(__dirname + '/dist/JSON');
-  if (fs.readdir(__dirname + '/dist/JSON')) {
+  const dealer = async state => {
+    return await fetch(
+      `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/state/${state}
+      `,
+      {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(res => res.json());
+  };
+
+  const zipcodes = require('zipcodes');
+  let arr = [];
+  Object.keys(zipcodes.states.abbr).map(async state => {
+    const result = await dealer(state);
+    // await fs.mkdir(__dirname + '/dist/JSON');
+    // if (fs.readdir(__dirname + '/dist/JSON')) {
     // loop through zip codes here
     // make fetch call for each zip code here
     // fs.writeFile(`${__dirname}/dist/JSON/${zip}.JSON`, res.data.zip) for each zipcode
-  }
+    // }
+    arr.push(result);
+  });
+  console.log(arr);
 }
