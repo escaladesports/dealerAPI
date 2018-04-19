7830c7c962dbb11f248cb11e24ec2a936d343a5e
Taylor <tbaustin1992@gmail.com>
2018-04-19 15:03:22 -0500

    add console logs

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import zipcodes from 'zipcodes';
 const fetchDealers = async function() {
   // if folder does not exist create JSON folder
   if (await !fs.readdir(__dirname + '/dist/JSON')) {
+    console.log('JSON FOLDER CREATED...');
     await fs.mkdir(__dirname + '/dist/JSON');
   }
 
@@ -17,6 +18,8 @@ const fetchDealers = async function() {
     // This will override older files, so essentailly "updating" it as well
     result.data.dealers.map(async dealer => {
       if ((await fs.readdir(__dirname + '/dist/JSON')) && dealer.zip) {
+        console.log('FILE CREATED WITH ZIPCODE');
+
         await fs.writeFile(__dirname + `/dist/JSON/${dealer.zip}.JSON`, dealer);
       }
     });
