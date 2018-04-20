f63d5ec28f09f5bb934678d0b4b0b6ec63a9ba7d
Taylor <tbaustin1992@gmail.com>
2018-04-20 13:03:02 -0500

    revamp build script

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import axios from 'axios';
 import zipcodes from 'zipcodes';
 
 const fetchDealers = async function() {
-  // Loop through states and pass into dealer API
   Object.keys(zipcodes.states.abbr).map(async state => {
     const result = await axios.get(
       `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/state/${state}`
     );
-    // Loop through dealers and create file name by zip with contents of dealer
-    // This will override older files, so essentailly "updating" it as well
+
     result.data.dealers.map(async dealer => {
       const file = __dirname + `dist/JSON/${dealer.zip}.json`;
       await fs.outputJson(file, dealer);
