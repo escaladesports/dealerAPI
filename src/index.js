9784d9080c3d8ee391ee83779bcdf054607fcb7b
Taylor <tbaustin1992@gmail.com>
2018-04-20 10:04:17 -0500

    add outputJson and single test

--- src/index.js
+++ src/index.js
@@ -8,21 +8,25 @@ const fetchDealers = async function() {
     console.log('JSON FOLDER CREATED...');
     await fs.mkdir(__dirname + '/dist/JSON');
   }
-
-  // Loop through states and pass into dealer API
-  Object.keys(zipcodes.states.abbr).map(async state => {
-    const result = await axios.get(
-      `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/state/${state}`
-    );
-    // Loop through dealers and create file name by zip with contents of dealer
-    // This will override older files, so essentailly "updating" it as well
-    result.data.dealers.map(async dealer => {
-      if ((await fs.readdir(__dirname + '/dist/JSON')) && dealer.zip) {
-        console.log('FILE CREATED WITH ZIPCODE...');
-        await fs.writeFile(__dirname + `/dist/JSON/${dealer.zip}.JSON`, dealer);
-      }
-    });
-  });
+  await fs.writeFile(
+    __dirname + `/dist/JSON/47711.JSON`,
+    'Hello there from 47711'
+  );
+  await fs.outputJson(`dist/JSON.47712.JSON`, { name: 'JP' });
+  // // Loop through states and pass into dealer API
+  // Object.keys(zipcodes.states.abbr).map(async state => {
+  //   const result = await axios.get(
+  //     `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/state/${state}`
+  //   );
+  //   // Loop through dealers and create file name by zip with contents of dealer
+  //   // This will override older files, so essentailly "updating" it as well
+  //   result.data.dealers.map(async dealer => {
+  //     if ((await fs.readdir(__dirname + '/dist/JSON')) && dealer.zip) {
+  //       console.log('FILE CREATED WITH ZIPCODE...');
+  //       await fs.writeFile(__dirname + `/dist/JSON/${dealer.zip}.JSON`, dealer);
+  //     }
+  //   });
+  // });
 };
 
 fetchDealers();
