9e950a7998e5ac72cbc8aceec246b792fd38cc76
Taylor <tbaustin1992@gmail.com>
2018-04-20 10:25:03 -0500

    try outputJson with api

--- src/index.js
+++ src/index.js
@@ -3,32 +3,18 @@ import axios from 'axios';
 import zipcodes from 'zipcodes';
 
 const fetchDealers = async function() {
-  // if folder does not exist create JSON folder
-  // console.log('JSON FOLDER CREATION STARTED...');
-  // if (await !fs.readdir(__dirname + '/dist/JSON')) {
-  //   await fs.mkdir(__dirname + '/dist/JSON');
-  // }
-  // console.log('WRITE FILE CREATION STARTED...');
-  // await fs.writeFile(
-  //   __dirname + `/dist/JSON/47711.JSON`,
-  //   'Hello there from 47711'
-  // );
-  console.log('OUTPUT JSON CREATION STARTED...');
-  await fs.outputJson(`dist/JSON.47712.JSON`, { name: 'JP' });
-  // // Loop through states and pass into dealer API
-  // Object.keys(zipcodes.states.abbr).map(async state => {
-  //   const result = await axios.get(
-  //     `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/state/${state}`
-  //   );
-  //   // Loop through dealers and create file name by zip with contents of dealer
-  //   // This will override older files, so essentailly "updating" it as well
-  //   result.data.dealers.map(async dealer => {
-  //     if ((await fs.readdir(__dirname + '/dist/JSON')) && dealer.zip) {
-  //       console.log('FILE CREATED WITH ZIPCODE...');
-  //       await fs.writeFile(__dirname + `/dist/JSON/${dealer.zip}.JSON`, dealer);
-  //     }
-  //   });
-  // });
+  // Loop through states and pass into dealer API
+  Object.keys(zipcodes.states.abbr).map(async state => {
+    const result = await axios.get(
+      `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/state/${state}`
+    );
+    // Loop through dealers and create file name by zip with contents of dealer
+    // This will override older files, so essentailly "updating" it as well
+    result.data.dealers.map(async dealer => {
+      const file = `dist/JSON/${dealer.zip}.json`;
+      await fs.outputJson(file, dealer);
+    });
+  });
 };
 
 fetchDealers();
