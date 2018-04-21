df28845c39dc0ec462ee19dd7ed8101c10af5680
tbaustin <tbaustin1992@gmail.com>
2018-04-21 09:59:55 -0500

    console abbr

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const fetchDealersByState = async function() {
         state.abbr
       }`
     );
-    console.log(result.data[state.abbr]);
+    console.log(result.data['IN']);
     // await fs.outputJson(
     //   path.resolve(__dirname, `../dist/state/${state.abbr}.json`),
     //   result.data[state.abbr]
