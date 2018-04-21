6f5efb2c31381c9fb53a4222007b71467d632edd
Taylor Austin <taylor@Taylors-MacBook-Pro.local>
2018-04-21 09:22:52 -0500

    testing api

--- src/index.js
+++ src/index.js
@@ -2,27 +2,31 @@ import fs from 'fs-extra';
 import path from 'path';
 import axios from 'axios';
 import zipcodes from 'zipcodes';
+import states from 'united-states';
+import cities from './cities.json';
 
-const fetchDealers = async function() {
-  await fs.outputJson(path.resolve(__dirname, '../dist/JSON/testFile.json'), {
-    name: 'Taylor'
-  });
-  Object.keys(zipcodes.states.abbr).map(async state => {
+const fetchDealersByState = async function() {
+  states.map(async state => {
     const result = await axios.get(
-      `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/state/${state}`
+      `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/state/${
+        state.name
+      }`
     );
-
-    result.data.dealers.map(async dealer => {
-      await fs.outputJson(
-        path.resolve(__dirname, `../dist/JSON/${dealer.zip}.json`),
-        dealer
-      );
-      console.log(path.resolve(__dirname, `../dist/JSON/${dealer.zip}.json`));
-    });
+    console.log(result.data);
+    // result.data.dealers.map(async dealer => {
+    //   await fs.outputJson(
+    //     path.resolve(__dirname, `../dist/JSON/${dealer.zip}.json`),
+    //     dealer
+    //   );
+    // });
   });
 };
 
-fetchDealers().then(async () => {
-  const rootFolder = await fs.readdir(path.resolve(__dirname, '../dist/JSON'));
-  console.log('List all directories inside of JSON -->', rootFolder);
-});
+// const fetchDealersByCity = async function() {
+
+// }
+
+fetchDealersByState();
+// fetchDealersByCity()
+// fetchDealersByZip()
+// fetchDealersByZipProx()
