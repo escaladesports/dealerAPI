58eefe92ec51a45e7c8abfc13556f603166fde2e
Taylor <tbaustin1992@gmail.com>
2018-04-23 14:27:35 -0500

    try parallel promises

--- dev/dev.js
+++ dev/dev.js
@@ -1,12 +1,1 @@
 import axios from 'axios';
-import goalrilla from '../dist/JSON/goalrilla.json';
-
-goalrilla.map(dealer => {
-  console.log(
-    'NAME --> |',
-    dealer.name,
-    '| ADDRESS --> |',
-    dealer.address,
-    '|'
-  );
-});
--- src/index.js
+++ src/index.js
@@ -7,27 +7,36 @@ import states from 'united-states';
 import brands from '../config/brands';
 
 const fetchDealers = async function() {
-  let data = [];
+  let promises = [];
   for (let brand of brands) {
     for (let { abbr } of states) {
-      let result = await axios.get(
-        `https://apis.escaladesports.com/v1/dealers/territory/${brand}/state/${abbr}/`
+      promises.push(
+        axios.get(
+          `https://apis.escaladesports.com/v1/dealers/territory/${brand}/state/${abbr}`
+        )
       );
-      result = result.data.dealers;
-      if (result.length > 0) {
-        result.forEach(dealer => {
-          const ifExist = data.filter(({ id }) => id === dealer.id);
-          if (ifExist.length === 0) {
-            data.push(dealer);
-          }
-        });
-      }
     }
-    await fs.outputJson(
-      path.resolve(__dirname, `../dist/JSON/${brand}.json`),
-      data
-    );
+    const results = await Promise.all(promises);
+    results.forEach(({ data: { dealers } }) => {
+      dealers.forEach(dealer => {
+        console.log(dealer.id);
+      });
+    });
+    // await fs.outputJson(
+    //   path.resolve(__dirname, `../dist/JSON/${brand}.json`),
+    //   data
+    // );
   }
 };
+
+// result = result.data.dealers;
+//       if (result.length > 0) {
+//         result.forEach(dealer => {
+//           const ifExist = data.filter(({ id }) => id === dealer.id);
+//           if (ifExist.length === 0) {
+//             data.push(dealer);
+//           }
+//         });
+//       }
 
 fetchDealers();
