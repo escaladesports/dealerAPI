321e66981b83b5885a27ae857d8a90524bd34dd3
Taylor <tbaustin1992@gmail.com>
2018-04-24 11:43:03 -0500

    add headers

--- dev/dev.js
+++ dev/dev.js
@@ -1,1 +1,12 @@
 import axios from 'axios';
+import zipcodes from 'zipcodes';
+import fetch from 'fetch-retry';
+
+fetch(url)
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+const radius = zipcodes.radius(47711, 100);
+
+console.log(radius);
