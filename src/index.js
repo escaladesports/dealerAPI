6262823c66bdaf3b8317845f4ff414b522a66e49
Taylor <tbaustin1992@gmail.com>
2018-04-27 09:56:30 -0500

    add console logs

--- src/index.js
+++ src/index.js
@@ -7,32 +7,26 @@ import states from 'united-states';
 import brands from '../config/brands';
 
 const fetchDealers = async function() {
-  let promises = [];
   let data = [];
 
   for (let brand of brands) {
-    promises.push(
-      axios.get(
-        `https://apistest.escaladesports.com/v1/dealers/list/${brand.name}/all`,
-        {
-          headers: {
-            Authorization: `API-KEY ${brand.key}`
-          }
+    const results = await axios.get(
+      `https://apistest.escaladesports.com/v1/dealers/list/${brand.name}/all`,
+      {
+        headers: {
+          Authorization: `API-KEY ${brand.key}`
         }
-      )
+      }
     );
-    const results = await Promise.all(promises);
-    console.log(results[0]);
 
-    results.forEach(({ data }, i) => {
-      console.log(i);
-      // dealers.forEach(dealer => {
-      //   const ifExist = data.find(({ id }) => id === dealer.id);
-      //   if (!ifExist) {
-      //     data.push(dealer);
-      //   }
-      // });
-    });
+    console.log(results.data);
+
+    //   for(let i in data){
+    //     const ifExist = data.find(({ id }) => id === data[i].id);
+    //     if (!ifExist){
+    //       data.push(data[i])
+    //     }
+    //   }
 
     // await fs.outputJson(
     //   path.resolve(__dirname, `../dist/JSON/${brand}.json`),
