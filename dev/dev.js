6d15c43f43ed92c4b6afb2567cbe3658106e1e4b
Taylor <tbaustin1992@gmail.com>
2018-11-12 09:32:36 -0600

    update brands

--- dev/dev.js
+++ dev/dev.js
@@ -1,12 +1,35 @@
-import axios from 'axios';
-import zipcodes from 'zipcodes';
-import fetch from 'fetch-retry';
+import fs from 'fs-extra'
+import path from 'path'
+import axios from 'axios'
+import brands from '../config/brands'
 
-fetch(url)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
+const fetchDealers = async function() {
+ let data = []
 
-const radius = zipcodes.radius(47711, 100);
+ for (let brand of brands) {
+  const results = await axios.get(
+   `https://apis.escaladesports.com/v1/dealers/list/${brand.name}/all`,
+   {
+    headers: {
+     Authorization: `API-KEY ${brand.key}`
+    }
+   }
+  )
 
-console.log(radius);
+  for (let i in results.data) {
+   const ifExist = data.find(({ id }) => id === results.data[i].id)
+   if (typeof results.data[i] === 'object') {
+    if (!ifExist) {
+     data.push(results.data[i])
+    }
+   }
+  }
+
+  await fs.outputJson(
+   path.resolve(__dirname, `../dist/JSON/${brand.name}.json`),
+   data
+  )
+ }
+}
+
+fetchDealers()
--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
-import fs from 'fs-extra';
-import path from 'path';
-import axios from 'axios';
-import fetch from 'fetch-retry';
-import zipcodes from 'zipcodes';
-import states from 'united-states';
-import brands from '../config/brands';
+import fs from 'fs-extra'
+import path from 'path'
+import axios from 'axios'
+import brands from '../config/brands'
 
 const fetchDealers = async function() {
-  let data = [];
+ let data = []
 
-  for (let brand of brands) {
-    const results = await axios.get(
-      `https://apis.escaladesports.com/v1/dealers/list/${brand.name}/all`,
-      {
-        headers: {
-          Authorization: `API-KEY ${brand.key}`
-        }
-      }
-    );
-
-    for (let i in results.data) {
-      const ifExist = data.find(({ id }) => id === results.data[i].id);
-      if (typeof results.data[i] === 'object') {
-        if (!ifExist) {
-          data.push(results.data[i]);
-        }
-      }
+ for (let brand of brands) {
+  const url = `https://apis.escaladesports.com/v1/dealers/list/${
+   brand.name
+  }/all`
+  console.log(url)
+  const results = await axios.get(url, {
+   headers: {
+    Authorization: `API-KEY ${brand.key}`
+   }
+  })
+  console.log(Object.keys(results.data).length)
+  for (let i in results.data) {
+   const ifExist = data.find(({ id }) => id === results.data[i].id)
+   if (typeof results.data[i] === 'object') {
+    if (!ifExist) {
+     data.push(results.data[i])
     }
-
-    await fs.outputJson(
-      path.resolve(__dirname, `../dist/JSON/${brand.name}.json`),
-      data
-    );
+   }
   }
-};
 
-fetchDealers();
+  await fs.outputJson(
+   path.resolve(__dirname, `../dist/JSON/${brand.name}.json`),
+   data
+  )
+  console.log(`Built page for ${brand.name}`)
+ }
+}
+
+fetchDealers()
