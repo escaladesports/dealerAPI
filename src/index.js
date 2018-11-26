3f17c58031dd2d9e8c3701d97dfc2ffb5f25fbb0
Taylor <tbaustin1992@gmail.com>
2018-11-26 08:47:06 -0600

    update to length for console

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import fs from "fs-extra";
-import path from "path";
-import axios from "axios";
+import fs from 'fs-extra'
+import path from 'path'
+import axios from 'axios'
 
-import brands from "../config/brands";
-import { getRegularDealers, getPlatinumDealers } from "./request/get";
+import brands from '../config/brands'
+import { getRegularDealers, getPlatinumDealers } from './request/get'
 
 const fetchDealers = async function() {
-  let data = [];
+ let data = []
 
-  for (let brand of brands) {
-    const regDealers = await getRegularDealers(brand[`name`], brand[`key`]);
-    const platDealers = await getPlatinumDealers(brand[`name`], brand[`key`]);
+ for (let brand of brands) {
+  const regDealers = await getRegularDealers(brand[`name`], brand[`key`])
+  const platDealers = await getPlatinumDealers(brand[`name`], brand[`key`])
 
-    console.log(`Regular Dealers`);
-    console.log(`------`);
-    console.log(regDealers);
-    console.log(`------`);
-    console.log(`Platinum Dealers`);
-    console.log(`------`);
-    console.log(platDealers);
-    console.log(`------`);
-    // await fs.outputJson(
-    //   path.resolve(__dirname, `../dist/JSON/${brand.name}.json`),
-    //   data
-    // );
-    console.log(`Built page for ${brand.name}`);
-  }
-};
+  console.log(`------`)
+  console.log(`Regular Dealers`)
+  console.log(regDealers)
+  console.log(`------`)
+  console.log(`------`)
+  console.log(`Platinum Dealers`)
+  console.log(Object.keys(platDealers))
+  console.log(`------`)
+  // await fs.outputJson(
+  //   path.resolve(__dirname, `../dist/JSON/${brand.name}.json`),
+  //   data
+  // );
+  console.log(`Built page for ${brand.name}`)
+ }
+}
 
-fetchDealers();
+fetchDealers()
