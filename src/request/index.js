be09ff003da497f5a902ca1fe99a7aa307f6bc6c
Taylor <tbaustin1992@gmail.com>
2018-11-26 15:04:09 -0600

    update api endpoint

--- src/request/index.js
+++ src/request/index.js
@@ -1,24 +1,41 @@
 import axios from 'axios'
 
+const api = {
+ getDealers: async (brand, brandKey, page) =>
+  await axios({
+   method: `get`,
+   url: `https://apistest.escaladesports.com/v1/dealers/list/${brand}/all/${page}`,
+   headers: {
+    Authorization: `API-KEY ${brandKey}`
+   }
+  }).then(res => res.data)
+}
+
 const request = {
  get: {
   dealers: async (brand, brandKey) => {
-   let page = 3
+   let page = 0
    let dealers = []
-   const res = await axios({
-    method: `get`,
-    url: `https://apistest.escaladesports.com/v1/dealers/list/${brand}/all/${page}`,
-    headers: {
-     Authorization: `API-KEY ${brandKey}`
-    }
-   }).then(res => res.data)
-   const { total, perpage, pages } = res
+   let res = await api.getDealers(brand, brandKey, page)
+   const { pages } = res
    Object.keys(res).forEach(key => {
     if (isNaN(key)) {
-     delete dealers[key]
+     delete res[key]
     }
    })
-   return res
+   dealers = [...dealers, ...res]
+   do {
+    page++
+    let paginationRes = await api.getDealers(brand, brandKey, page)
+    Object.keys(paginationRes).forEach(key => {
+     if (isNaN(key)) {
+      delete paginationRes[key]
+     }
+    })
+    dealers = [...dealers, ...paginationRes]
+   } while (page <= pages)
+
+   return dealers
   }
  },
  post: {},
